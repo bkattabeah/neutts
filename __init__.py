986b4be3d1eeb49dce7c263c8804e91affcb8a3a
Beah Katta <bkattabeah@users.noreply.github.com>
2026-05-09 06:01:46 +0800

    expose __version__ in package __init__ for easier version checking

--- __init__.py
+++ __init__.py
@@ -1,1 +1,7 @@
 from neutts.neutts import NeuTTS, BACKBONE_LANGUAGE_MAP  # noqa
+
+# Also expose the version for convenience
+try:
+    from neutts._version import __version__
+except ImportError:
+    __version__ = "unknown"
